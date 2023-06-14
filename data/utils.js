8409097751da7f1a0ac43d55305f699b634331ba
SekoViper <damehlivingstone1@gmail.com>
2023-06-14 21:55:32 +0000

    Add policy data

---
+++ data/utils.js
@@ -0,0 +1,22 @@
+const policies = [  
+    {  
+        "id":"1",
+        "fileName":"Acceptable Use Policy",
+        "desc":"This policy provides guidelines for acceptable use of company-provided devices and networks. It applies to all employees, contractors, and vendors who use company-provided devices or access company networks.",
+    },
+    {
+        "id":"2",
+        "fileName":"Access Control Policy",
+        "desc":"This policy provides guidelines for granting access to company networks and systems. It applies to all employees, contractors, and vendors who use company networks or systems.",
+    },
+    {
+        "id":"3",
+        "fileName":"BYOD Policy",
+        "desc":"This policy provides guidelines for employees who use their own devices for work purposes. It applies to all employees, contractors, and vendors who use their own devices to access company networks or systems.",     
+    },
+    {
+        "id":"4",
+        "fileName":"Data Clarification Policy",
+        "desc":"This policy provides guidelines for responding to a data breach. It applies to all employees, contractors, and vendors who handle company data or respond to a data breach.",
+    },
+]
