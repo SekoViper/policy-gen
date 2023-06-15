8b48f7ebc72a3af6b67aac0901d738d7c9892eff
SekoViper <damehlivingstone1@gmail.com>
2023-06-15 09:36:58 +0000

    Policy generation page

---
+++ pages/[id].js
@@ -0,0 +1,13 @@
+import React from "react";
+import PolicyForm from "../components/policy-form";
+
+const Policies = () => {
+  return (
+    <>
+      <h1>Generate Policy</h1>
+      <PolicyForm />
+    </>
+  );
+};
+
+export default Policies;
