6b7a832f0657622d9348c4ff4e1b8ea2f9f64f64
SekoViper <damehlivingstone1@gmail.com>
2023-06-15 17:25:39 +0000

    Modify to add Heading

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,18 @@
 import React from "react";
 import PolicyForm from "../components/policy-form";
+import { useRouter } from "next/router";
 
 const Policies = () => {
+  const { id } = useRouter().query;
+  const removeExtension = id.replace(".hbs", "");
+  const removeHyphen = removeExtension.replace(/-/g, " ");
+  const words = removeHyphen.split(" ");
+  const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  const fileName = capitalizedWords.join(" ");
+
   return (
-    <div className="container mx-auto py-4">
-      <h1 className="text-2xl font-bold mb-4">Generate Policy</h1>
+    <div className="container mt-12 mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4 text-center ">Generate {fileName}</h1>
       <PolicyForm />
     </div>
   );
