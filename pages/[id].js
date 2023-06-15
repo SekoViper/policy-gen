8cf687e9e85b9f2a3b22b9259191fdafa76a18b1
SekoViper <damehlivingstone1@gmail.com>
2023-06-15 16:51:25 +0000

    Add styling

--- pages/[id].js
+++ pages/[id].js
@@ -3,10 +3,10 @@ import PolicyForm from "../components/policy-form";
 
 const Policies = () => {
   return (
-    <>
-      <h1>Generate Policy</h1>
+    <div className="container mx-auto py-4">
+      <h1 className="text-2xl font-bold mb-4">Generate Policy</h1>
       <PolicyForm />
-    </>
+    </div>
   );
 };
 
